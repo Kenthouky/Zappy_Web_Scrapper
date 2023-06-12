6301da03d4dabbfe45c4a13b687a8077c284e983
Dollars Kenthouky <dollarskenthouky@gmail.com>
2023-06-12 19:10:09 +0100

    zappy

---
+++ index.js
@@ -0,0 +1,51 @@
+// Import necessary modules
+const express = require('express');
+const axios = require('axios');
+const cheerio = require('cheerio');
+const fs = require('fs');
+
+// Create Express app
+const app = express();
+
+// Set up a route for the homepage
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/index.html');
+});
+
+// Set up a route for handling form submissions
+app.get('/scrape', (req, res) => {
+  const url = req.query.url;
+
+  // Send a GET request to the specified URL
+  axios.get(url)
+    .then(response => {
+      // Load the HTML content of the website
+      const $ = cheerio.load(response.data);
+      let text = '';
+
+      // Extract all the text from the website
+      $('body').each((index, element) => {
+        text += $(element).text();
+      });
+
+      // Save the scraped text to a file
+      fs.writeFile('scraped_text.txt', text, err => {
+        if (err) {
+          console.error(err);
+          res.send('Error occurred while scraping.');
+        } else {
+          res.download('scraped_text.txt');
+        }
+      });
+    })
+    .catch(error => {
+      console.error(error);
+      res.send('Error occurred while scraping.');
+    });
+});
+
+// Start the server
+const port = 3000;
+app.listen(port, () => {
+  console.log(`Server running on http://localhost:${port}`);
+});
