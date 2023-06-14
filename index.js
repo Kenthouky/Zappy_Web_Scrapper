a659e8a9149c0cb7180bd93832e01f1cd2b943ab
Oliver Samuel <104834870+Tecnophille@users.noreply.github.com>
2023-06-14 18:01:15 +0100

    Update index.js

--- index.js
+++ index.js
@@ -9,10 +9,10 @@ const app = express();
 
 app.use(express.static('public'));
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname,'index.html'));
+  res.sendFile(path.join(__dirname, 'index.html'));
 });
 app.get('/scrape-web', (req, res) => {
-  res.sendFile(path.join(__dirname,'scrape.html'));
+  res.sendFile(path.join(__dirname, 'scrape.html'));
 });
 
 app.get('/scrape', async (req, res) => {
